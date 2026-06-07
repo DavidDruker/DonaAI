59874480fccb55bf631fec9bc08136584411cc3a
David Druker <dave.druker@gmail.com>
2026-06-07 16:50:09 -0400

    fixed backend connectivity

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { registerRootComponent } from "expo";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import SecretaryApp from "./src/SecretaryApp";
 
-registerRootComponent(SecretaryApp);
+function Root() {
+  return (
+    <SafeAreaProvider>
+      <SecretaryApp />
+    </SafeAreaProvider>
+  );
+}
+
+registerRootComponent(Root);
